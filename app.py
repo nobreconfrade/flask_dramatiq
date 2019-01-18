cd9a9bc64a5032e81aeca8a6a2d2372ccc767e65
William Pereira <wpereira@esss.co>
2019-01-18 17:13:09 -0200

    actor for prime search

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template
 
+import dramatiq
+
 app = Flask(__name__)
 
+@dramatiq.actor
+def prime_search():
+    lower = 900
+    upper = 1000
 
+    values = []
+
+    # print("Prime numbers between",lower,"and",upper,"are:")
+
+    for num in range(lower,upper + 1):
+       # prime numbers are greater than 1
+       if num > 1:
+           for i in range(2,num):
+               if (num % i) == 0:
+                   break
+           else:
+               values.append(num)
 
 @app.route("/")
 def index():
