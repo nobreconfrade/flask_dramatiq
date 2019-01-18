50dd5c8efc27ecd6fc19c3f3e87470051a475146
William Pereira <wpereira@esss.co>
2019-01-18 17:46:45 -0200

    added broker port

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+
+from dramatiq.brokers.redis import RedisBroker
 
 import dramatiq
 
 app = Flask(__name__)
 
+values = []
+
+redis_broker = RedisBroker(host="redis", port=6379)
+dramatiq.set_broker(redis_broker)
+
 @dramatiq.actor
 def prime_search():
     lower = 900
     upper = 1000
-
-    values = []
 
     # print("Prime numbers between",lower,"and",upper,"are:")
 
@@ -22,6 +27,11 @@ def prime_search():
            else:
                values.append(num)
 
+@app.route("/start_dramatiq")
+def run():
+    prime_search.send()
+    return redirect("/")
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", values=values)
