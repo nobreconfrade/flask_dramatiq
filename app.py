8e1f78342577d366d21bfb76540feb90c157e748
William Pereira <wpereira@esss.co>
2019-01-21 17:04:50 -0200

    sync error message

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dramatiq import Message
+from dramatiq.middleware import TimeLimitExceeded
 from dramatiq.results import Results
 from flask import Flask, render_template, redirect, request
 
@@ -15,39 +16,62 @@ dramatiq_results = RedisBackend()
 redis_broker.add_middleware(Results(backend=dramatiq_results))
 dramatiq.set_broker(redis_broker)
 
+log = dramatiq.logging.get_logger(__name__)
+
+TIME_LIMIT_ACTOR = 5000
+
 values = []
 message = []
 
-@dramatiq.actor(store_results=True)
+@dramatiq.actor(store_results=True, time_limit=TIME_LIMIT_ACTOR)
 def prime_search(start_v, end_v):
-    lower = start_v
-    upper = end_v
-    values = []
+    values = {}
+    try:
+        lower = start_v
+        upper = end_v
 
-    for num in range(lower,upper + 1):
-       # prime numbers are greater than 1
-       if num > 1:
-           for i in range(2,num):
-               if (num % i) == 0:
-                   break
-           else:
-               values.append(num)
-    # print(values)
-    return values
+        values['val'] = []
+
+        for num in range(lower, upper + 1):
+            # prime numbers are greater than 1
+            if num > 1:
+                for i in range(2, num):
+                    if (num % i) == 0:
+                        break
+                else:
+                    values['val'].append(num)
+        return values
+
+    except TimeLimitExceeded:
+        actor = 'prime_search'
+        message = f'Time limit of {TIME_LIMIT_ACTOR} ms exceeded on: {actor}'
+        values['val'] = []
+        values['err'] = message
+        log.info(values['err'])
+
+        return values
+
 
 @app.route("/start_dramatiq")
 def run():
     start = int(request.args.get('start'))
     end = int(request.args.get('end'))
     message.append(prime_search.send(start, end))
-    # print(dir(message))
-    # print(message.get_result())
     return redirect("/")
+
+def error_alert(message):
+    print('Error while processing.')
+    print('Message: ', message)
 
 @app.route("/check")
 def check_results():
     for el in message:
-        print(el.get_result())
+        result = el.get_result()
+        if 'err' not in result:
+            print(result['val'])
+            values.append(el.get_result()['val'])
+        else:
+            error_alert(result['err'])
     return redirect("/")
 
 @app.route("/")
