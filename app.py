86daba3be32a22647489b804a32470b9446aacac
William Pereira <wpereira@esss.co>
2019-01-21 14:45:26 -0200

    working and sending messages throw backend

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-from flask import Flask, render_template, redirect
+from dramatiq import Message
+from dramatiq.results import Results
+from flask import Flask, render_template, redirect, request
 
 from dramatiq.brokers.redis import RedisBroker
+from dramatiq.results.backends import RedisBackend
 
 import dramatiq
 
 app = Flask(__name__)
 
-values = []
+redis_broker = RedisBroker(host="localhost", port=6379)
+dramatiq_results = RedisBackend()
 
-redis_broker = RedisBroker(host="redis", port=6379)
+redis_broker.add_middleware(Results(backend=dramatiq_results))
 dramatiq.set_broker(redis_broker)
 
-@dramatiq.actor
-def prime_search():
-    lower = 900
-    upper = 1000
+values = []
+message = []
 
-    # print("Prime numbers between",lower,"and",upper,"are:")
+@dramatiq.actor(store_results=True)
+def prime_search(start_v, end_v):
+    lower = start_v
+    upper = end_v
+    values = []
 
     for num in range(lower,upper + 1):
        # prime numbers are greater than 1
@@ -26,10 +32,22 @@ def prime_search():
                    break
            else:
                values.append(num)
+    # print(values)
+    return values
 
 @app.route("/start_dramatiq")
 def run():
-    prime_search.send()
+    start = int(request.args.get('start'))
+    end = int(request.args.get('end'))
+    message.append(prime_search.send(start, end))
+    # print(dir(message))
+    # print(message.get_result())
+    return redirect("/")
+
+@app.route("/check")
+def check_results():
+    for el in message:
+        print(el.get_result())
     return redirect("/")
 
 @app.route("/")
